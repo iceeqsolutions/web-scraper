5864e61fd9af8cafa7c7651a9db66262f302c4ed
Fleege <76089935+pfleege@users.noreply.github.com>
2024-10-31 08:13:20 +0200

    ADD beautifulsoup4 + test script for parsing html
    - Create a search string to search site based on user input
    - Parse the html of the site and return selected value and display it
    in the browser (index.html)

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    searchTerm = "4080"
+    url = f"https://www.data-systems.fi/?s={searchTerm}&post_type=product"
+    result = requests.get(url).text
+    doc = BeautifulSoup(result, "html.parser")
+    page_text = doc.find('p', class_="woocommerce-result-count").text
+
+    # print(page_text)
+
+    return render_template("index.html", page_text=page_text)
 
 if __name__ == "__main__":
     app.run(debug=True)
